9f72a4a2cb91bd67afc120e915ed9d61640470a7
Eric Zuckerman <ericzuckerman@Erics-MBP.att.net>
2016-10-08 16:13:55 -0700

    changed config for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,17 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Bor77scht',
-    database: 'spaceship_db'
-});
+var connection ;
+
+if (process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+	connection = mysql.createConnection({
+    	host: 'localhost',
+    	user: 'root',
+    	password: 'Bor77scht',
+    	database: 'spaceship_db'
+	});
+}
 
 
 connection.connect(function(err) {
